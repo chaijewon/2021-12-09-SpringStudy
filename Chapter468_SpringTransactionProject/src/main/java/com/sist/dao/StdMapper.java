f915ffb32ceb16dc07ca733e33ebd77f9bdb65d7
user <user@DESKTOP-COJ9858>
2022-03-25 08:58:35 +0900

    111

---
+++ Chapter468_SpringTransactionProject/src/main/java/com/sist/dao/StdMapper.java
@@ -0,0 +1,12 @@
+package com.sist.dao;
+import java.util.*;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+public interface StdMapper {
+  @Select("SELECT * FROM std_input")
+  public List<StdVO> stdListData();
+  @Insert("INSERT INTO std_input VALUES("
+		 +"#{no},#{name},#{kor},#{eng},#{math})")
+  public void stdInsert(StdVO vo);
+}
