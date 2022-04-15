bfab4dac16c42bc41ae234f8f1a43e75671c0023
chaijewon <vcandjava@nate.com>
2022-04-15 12:22:33 +0900

    1111

---
+++ SpringBootMyBatisProject2/src/main/java/com/sist/music/controller/MusicController.java
@@ -0,0 +1,47 @@
+package com.sist.music.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
+import com.sist.music.service.*;
+import com.sist.music.vo.*;
+@Controller
+public class MusicController {
+    @Autowired
+	private MusicService service;
+   
+    @GetMapping("/")
+    public String musicListData(String page,Model model)
+    {
+    	System.out.println(this);
+    	System.out.println("연결");
+    	if(page==null)
+    		page="1";
+    	int curpage=Integer.parseInt(page);
+    	Map map=new HashMap();
+    	map.put("cno", 1);
+    	int size=20;
+    	int start=(size*curpage)-size;
+    	map.put("start", start);
+    	map.put("size", size);
+    	List<MusicVO> list=service.musicListData(map);
+    	int totalpage=service.musicTotalPage(1);
+    	
+    	model.addAttribute("list", list);
+    	model.addAttribute("curpage", curpage);
+    	model.addAttribute("totalpage", totalpage);
+    	return "main";
+    }
+   
+}
+
+
+
+
+
+
+
+
